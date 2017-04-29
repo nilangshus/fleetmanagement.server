630484c6ed98ee567138afbba3d58e23aed19178
Nilangshu <hu_nilangshu@gmail.com>
2017-04-30 01:54:11 +0530

    write to a file

--- src/main/java/com/fleetmanagement/Locations.java
+++ src/main/java/com/fleetmanagement/Locations.java
@@ -1,19 +1,27 @@
 package com.fleetmanagement;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.google.gson.JsonObject;
-
 @RestController
 public class Locations {
 	
+	private static final String LOCATIONS = "locations.txt";
+	
 	@RequestMapping(path="/locations/{latitude_longitude}/test", method = RequestMethod.GET)
-	public @ResponseBody FleetResponse locations(@PathVariable String latitude_longitude) {
+	public @ResponseBody FleetResponse locations(@PathVariable String latitude_longitude) throws IOException {
 
+		FileWriter fw = new FileWriter(LOCATIONS,true);
+		fw.write(latitude_longitude+"\n\r");
+		fw.close();
+			
 		FleetResponse fleetResponse = new FleetResponse();
 		fleetResponse.setLatitudeLongitude(latitude_longitude);
 		return fleetResponse;
