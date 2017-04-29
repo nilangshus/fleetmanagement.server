9106d9893bf1137aeced43ed2d924dd32ffe56d0
Nilangshu <hu_nilangshu@gmail.com>
2017-04-30 01:17:58 +0530

    changes

---
+++ src/main/java/com/fleetmanagement/FleetResponse.java
@@ -0,0 +1,17 @@
+package com.fleetmanagement;
+
+public class FleetResponse {
+	
+	private String latitudeLongitude;
+
+	public String getLatitudeLongitude() {
+		return latitudeLongitude;
+	}
+
+	public void setLatitudeLongitude(String latitudeLongitude) {
+		this.latitudeLongitude = latitudeLongitude;
+	}
+	
+	
+
+}
