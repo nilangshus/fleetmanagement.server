1663eb0ffa18c4fbb9b2f9a1bf3b5d92f683c53e
Nilangshu <hu_nilangshu@gmail.com>
2017-04-30 01:15:00 +0530

    changes

--- src/main/java/com/fleetmanagement/Locations.java
+++ src/main/java/com/fleetmanagement/Locations.java
@@ -3,14 +3,20 @@ package com.fleetmanagement;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.google.gson.JsonObject;
 
 @RestController
 public class Locations {
 	
 	@RequestMapping(path="/locations/{latitude_longitude}/test", method = RequestMethod.GET)
-	public void locations(@PathVariable String latitude_longitude) {
-		System.out.println("latitude:: "+ latitude_longitude);
+	public @ResponseBody FleetResponse locations(@PathVariable String latitude_longitude) {
+
+		FleetResponse fleetResponse = new FleetResponse();
+		fleetResponse.setLatitudeLongitude(latitude_longitude);
+		return fleetResponse;
 	}
 
 }
